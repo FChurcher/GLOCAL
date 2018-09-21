6923fddd2678a7c63ac265ba40537dd9fb21d9bc
Falco <flckirchner@gmail.com>
2018-09-21 14:05:35 +0200

    updated: LMATFU

--- LMATFU/src/main_LMATFU.java
+++ LMATFU/src/main_LMATFU.java
@@ -10,7 +10,7 @@ public class main_LMATFU {
 	public static void main(String[] args) {
 		System.out.println("asd");
 		FileExplorer.refresh();
-		Job j = JobBuilder.buildJob("touch" + FileExplorer.toAlignDir + File.separator + "add.fasta");
+		Job j = JobBuilder.buildJob("touch " + FileExplorer.toAlignDir + File.separator + "add.fasta");
 		FileExplorer.refresh();
 		Job j2 = JobBuilder.buildJob("sleep 10");
 	}
