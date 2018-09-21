3e8804b31675b8e3f4d9d7c2b7d3773e23ae6abb
Falco <flckirchner@gmail.com>
2018-09-21 14:00:03 +0200

    updated: LMATFU

--- LMATFU/src/io/FileExplorer.java
+++ LMATFU/src/io/FileExplorer.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class FileExplorer {
-	public static final File toAlignDir = new File(File.separator + "LMATFU" + File.separator + "to_align");
+	public static final File toAlignDir = new File("LMATFU" + File.separator + "to_align" + File.separator);
 	
 	public static ArrayList<String> fileNamesToAlign = new ArrayList<>();
 	
 	public static void refresh() {
 		System.out.println(toAlignDir);
+		System.out.println(toAlignDir.listFiles());
 		for (File file : toAlignDir.listFiles()) {
 			String name = file.getName().substring(0, file.getName().lastIndexOf('.'));
 			if (!fileNamesToAlign.contains(name)) {
