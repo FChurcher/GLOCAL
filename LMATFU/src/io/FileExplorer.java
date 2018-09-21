96e575fdd99c1fc05506f4f729412cd2340b6ece
Falco <flckirchner@gmail.com>
2018-09-21 14:11:32 +0200

    updated: LMATFU

--- LMATFU/src/io/FileExplorer.java
+++ LMATFU/src/io/FileExplorer.java
@@ -7,6 +7,27 @@ public class FileExplorer {
 	public static final File toAlignDir = new File("LMATFU" + File.separator + "to_align");
 	
 	public static ArrayList<String> fileNamesToAlign = new ArrayList<>();
+	public static Process refreshingProcess;
+	public static boolean run;
+	
+	public static void start() {
+		run = true;
+		refresh();
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				while (run) {
+					refresh();
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {e.printStackTrace();}
+				}
+			}
+		});
+	}
+	
+	public static void stop() {
+		run = false;
+	}
 	
 	public static void refresh() {
 		System.out.println(toAlignDir);
--- LMATFU/src/main_LMATFU.java
+++ LMATFU/src/main_LMATFU.java
@@ -9,9 +9,8 @@ public class main_LMATFU {
 	
 	public static void main(String[] args) {
 		System.out.println("asd");
-		FileExplorer.refresh();
+		FileExplorer.start();
 		Job j = JobBuilder.buildJob("touch " + FileExplorer.toAlignDir + File.separator + "add.fasta");
-		FileExplorer.refresh();
 		Job j2 = JobBuilder.buildJob("sleep 10");
 	}
 }
