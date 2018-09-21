9555b311aee1d17555ceab059d2272159f27a96a
Falco <flckirchner@gmail.com>
2018-09-21 14:01:55 +0200

    updated: LMATFU

--- LMATFU/src/main_LMATFU.java
+++ LMATFU/src/main_LMATFU.java
@@ -7,6 +7,8 @@ public class main_LMATFU {
 	public static void main(String[] args) {
 		System.out.println("asd");
 		FileExplorer.refresh();
+		Job j = JobBuilder.buildJob("touch" + FileExplorer.toAlignDir + "add.fasta");
+		FileExplorer.refresh();
 		Job j = JobBuilder.buildJob("sleep 10");
 	}
 }
