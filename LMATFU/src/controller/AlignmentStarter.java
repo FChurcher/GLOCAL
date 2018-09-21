33d1156d76da4e0ae20568d28b7056a742f7ec19
Falco <flckirchner@gmail.com>
2018-09-21 14:45:24 +0200

    updated: LMATFU

--- LMATFU/src/controller/AlignmentStarter.java
+++ LMATFU/src/controller/AlignmentStarter.java
@@ -8,7 +8,7 @@ public class AlignmentStarter {
 	
 	public static void startGlocal(String fileName) {
 		System.out.println("starting GLOCAL");
-		Job j = JobBuilder.buildJob("java -Xmx50G -Xms200G -jar GLOCAL.jar " + "LMATFU" + File.separator + "to_align" + File.separator + fileName);
+		Job j = JobBuilder.buildJob("java -Xms50G -Xmx200G -jar GLOCAL.jar " + "LMATFU" + File.separator + "to_align" + File.separator + fileName);
 	}
 
 }
