5961a63491483febe0f6ed1b92dd8bc762a24faa
Falco Kirchner <mai13byy@studserv.uni-leipzig.de>
2018-09-22 14:59:56 +0200

    updated: LMATFU

--- LMATFU/src/main_LMATFU.java
+++ LMATFU/src/main_LMATFU.java
@@ -1,10 +1,12 @@
 import controller.AlignmentStarter;
 import controller.JobBuilder;
+import controller.JobHolder;
 import io.FileCollector;
 
 public class main_LMATFU {
 	
 	public static void main(String[] args) {
 		FileCollector.start();
+		JobHolder.start();
 	}
 }
